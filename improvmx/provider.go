c70b7a10d6d176535aca54e77bfe85e89f0e0728
Andreas K. Weber <code@andreas-weber.me>
2021-10-17 21:23:52 +0200

    Improves readability.

--- improvmx/provider.go
+++ improvmx/provider.go
@@ -15,7 +15,13 @@ type Meta struct {
 
 func Provider() *schema.Provider {
 	p := &schema.Provider{
-		Schema: map[string]*schema.Schema{"token": {Type: schema.TypeString, Required: true, DefaultFunc: schema.EnvDefaultFunc("IMPROVMX_API_TOKEN", nil), Description: "The API token for API operations."}},
+		Schema: map[string]*schema.Schema{
+			"token": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("IMPROVMX_API_TOKEN", nil),
+				Description: "The API token for API operations."},
+		},
 		ResourcesMap: map[string]*schema.Resource{
 			"improvmx_domain":          resourceDomain(),
 			"improvmx_email_forward":   resourceEmailForward(),
